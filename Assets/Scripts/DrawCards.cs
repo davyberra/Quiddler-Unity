2d28bfa241507dc00f12e8a292058942d9b373ae
unknown <davyberra@gmail.com>
2021-03-13 14:26:02 -0800

    first commit

---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject Card;
+    public GameObject playerHandArea;
+    public GameObject faceDownPile;
+    public GameObject discardPile;
+    public GameObject goDownArea;
+    
+    
+    
+
+
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        GameObject playerCard = Instantiate(Card, new Vector3(0, 0, 0), Quaternion.identity);
+        
+        playerCard.transform.SetParent(playerHandArea.transform, false);
+    }
+}
